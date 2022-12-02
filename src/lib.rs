40d97d1e491ec3cca2d983cd3444fb9617239c55
CoderEx24 <coderex24@protonmail.com>
2022-12-02 17:06:42 +0200

    formatted lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,41 +6,25 @@ type ParsedAsm = (HashMap<String, u16>, Vec<String>);
 /// parses a string that contains a numerical value, and returns it as a 2-byte number
 pub fn parse_value(str_val: &str) -> Result<u16, Box<dyn std::error::Error>> {
     use std::u16;
-    
+
     if str_val.starts_with('\'') && str_val.ends_with('\'') {
         return Ok(str_val.as_bytes()[1] as u16);
-
     }
-
 
     let val_lower = str_val.to_ascii_lowercase();
     let val = val_lower.as_str();
-    
-    Ok (
 
-    if val.len() > 2 && &val[0..2] == "0x" {
+    Ok(if val.len() > 2 && &val[0..2] == "0x" {
         let hex_value = &val[2..].to_uppercase();
 
         u16::from_str_radix(hex_value, 16)?
-           // .expect(
-           // format!("[ERROR] failed to parse {}", val).as_str()
-        //)
-
     } else if val.len() > 1 && &val[0..1] == "0" {
         let octal_value = &val[1..];
-        
-        u16::from_str_radix(octal_value, 8)?
-           // .expect(
-           //  format!("[ERROR] failed to parse {}", val).as_str()
-        //)
 
+        u16::from_str_radix(octal_value, 8)?
     } else {
         val.parse()?
-           // .expect(
-           // format!("[ERROR] failed to parse {}", val).as_str()
-        //)
-   }
-    )
+    })
 }
 
 /// # parse_asm
@@ -51,29 +35,25 @@ pub fn parse_asm(contents: String) -> Result<ParsedAsm, String> {
     let mut code = Vec::new();
     let mut is_data = true;
 
-
     for (i, line) in contents
-                        .to_ascii_lowercase()
-                        .trim()
-                        .split('\n')
-                        .into_iter()
-                        .filter(|v| !v.is_empty())
-                        .map(|v| v.trim_start().trim_end())
-                        .enumerate() {
-
+        .to_ascii_lowercase()
+        .trim()
+        .split('\n')
+        .into_iter()
+        .filter(|v| !v.is_empty())
+        .map(|v| v.trim_start().trim_end())
+        .enumerate()
+    {
         if line.trim_end_matches(':').to_lowercase() == "data" {
             is_data = true;
             continue;
-
         } else if line.trim_end_matches(':').to_lowercase() == "text" {
             is_data = false;
             continue;
-
         } else if line.ends_with(':') {
             let label = line.trim_end_matches(':');
             data.insert(String::from(label), (i + 1) as u16);
             continue;
-
         }
 
         if is_data {
@@ -82,12 +62,9 @@ pub fn parse_asm(contents: String) -> Result<ParsedAsm, String> {
             let value = parse_value(parts[1]).unwrap();
 
             data.insert(String::from(label), value);
-
-        } else if ! line.ends_with(':') {
+        } else if !line.ends_with(':') {
             code.push(String::from(line.trim()));
-
         }
-
     }
 
     Ok((data, code))
@@ -95,7 +72,7 @@ pub fn parse_asm(contents: String) -> Result<ParsedAsm, String> {
 
 /// # produce_machine_code
 /// takes the data and code sections and produces equivlent machine code
-fn produce_machine_code((data, code): ParsedAsm) -> Result<Vec<u16>, &'static str> {
+pub fn produce_machine_code((data, code): ParsedAsm) -> Result<Vec<u16>, &'static str> {
     if data.len() + code.len() > 4096 {
         return Err("[ERROR] The program exceeds the max size allowed (4096 16-bit words)");
     }
@@ -126,18 +103,10 @@ fn produce_machine_code((data, code): ParsedAsm) -> Result<Vec<u16>, &'static st
         ("ski", 0xf200),
         ("sko", 0xf100),
         ("ion", 0xf080),
-        ("iof", 0xf040)
+        ("iof", 0xf040),
     ]);
 
-    let mri = [
-        "add",
-        "and",
-        "lda",
-        "sta",
-        "bun",
-        "bsa",
-        "isz",
-    ];
+    let mri = ["add", "and", "lda", "sta", "bun", "bsa", "isz"];
 
     machine_code.push(0x4000 | (1 + data.len() as u16));
 
@@ -145,30 +114,30 @@ fn produce_machine_code((data, code): ParsedAsm) -> Result<Vec<u16>, &'static st
 
     for line in code.iter() {
         let parts: Vec<&str> = line.split(' ').collect();
-        let instruction = 
-            if mri.contains(&parts[0]) {
-
-                let opcode  = translation_table.get(parts[0]).unwrap();
-                let address = data.get(parts[1]).cloned().unwrap_or_else(
-                    || parse_value(parts[1]).unwrap()
-                );
-                let indirect = if parts.len() == 3 
-                                    && parts[2].to_lowercase() == "i" { 1 } else { 0 };
-
-                address | (opcode << 12) | (indirect << 15)
-
+        let instruction = if mri.contains(&parts[0]) {
+            let opcode = translation_table.get(parts[0]).unwrap();
+            let address = data
+                .get(parts[1])
+                .cloned()
+                .unwrap_or_else(|| parse_value(parts[1]).unwrap());
+            let indirect = if parts.len() == 3 && parts[2].to_lowercase() == "i" {
+                1
             } else {
-                translation_table.get(parts[0]).cloned().expect(
-                    format!("[ERROR] {} is not a recgonized instruction", parts[0]).as_str()
-                )
+                0
             };
 
-        machine_code.push(instruction);
+            address | (opcode << 12) | (indirect << 15)
+        } else {
+            translation_table
+                .get(parts[0])
+                .cloned()
+                .expect(format!("[ERROR] {} is not a recgonized instruction", parts[0]).as_str())
+        };
 
+        machine_code.push(instruction);
     }
 
     Ok(machine_code)
-    
 }
 
 #[cfg(test)]
@@ -177,17 +146,15 @@ mod tests {
 
     #[test]
     fn test_parse_value() {
-        assert_eq!(12, parse_value( "12").unwrap());
+        assert_eq!(12, parse_value("12").unwrap());
         assert_eq!(12, parse_value("014").unwrap());
         assert_eq!(12, parse_value("0xC").unwrap());
         assert_eq!(12, parse_value("0xc").unwrap());
-        
     }
 
     #[test]
     fn test_parse_asm() {
-        let contents = 
-            "data:    \n \
+        let contents = "data:    \n \
                 a 1   \n \
                 b 2   \n \
              text:    \n \
@@ -203,8 +170,8 @@ mod tests {
 
         assert_eq!("lda a", code[0]);
         assert_eq!("add b", code[1]);
-        assert_eq!("out",   code[2]);
-        assert_eq!("hlt",   code[3]);
+        assert_eq!("out", code[2]);
+        assert_eq!("hlt", code[3]);
 
         assert_eq!(1, *data.get("a").unwrap());
         assert_eq!(2, *data.get("b").unwrap());
@@ -214,16 +181,13 @@ mod tests {
 
     #[test]
     fn test_produce_machine_code() {
-        let data = HashMap::from([
-            ("a".to_string(), 1),
-            ("b".to_string(), 2)
-        ]);
+        let data = HashMap::from([("a".to_string(), 1), ("b".to_string(), 2)]);
 
         let code = vec![
             "lda a".to_string(),
             "add b".to_string(),
             "out".to_string(),
-            "hlt".to_string()
+            "hlt".to_string(),
         ];
 
         let machine_code = produce_machine_code((data, code)).unwrap();
